0b5e09579e7c7e11bbfcfd8d3aa2be23903bd209
lucioacri <lucioacri@gmail.com>
2025-04-14 14:04:11 +0200

    add first snack

--- js/script.js
+++ js/script.js
@@ -2,6 +2,41 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
+const bikesArray = [
+  {
+    name: "First Bike",
+    weight: 50,
+  },
+  {
+    name: "Second Bike",
+    weight: 40,
+  },
+  {
+    name: "Third Bike",
+    weight: 30,
+  },
+  {
+    name: "Fourth Bike",
+    weight: 20,
+  },
+];
+
+const bikesWeight = [];
+
+for (const bikes of bikesArray) {
+  bikesWeight.push(bikes.weight);
+}
+
+let smallestWeight = bikesWeight[0];
+console.log(bikesWeight);
+
+for (let i = 0; i < bikesWeight.length; i++) {
+  if (bikesWeight[i] < smallestWeight) {
+    smallestWeight = bikesWeight[i];
+  }
+}
+console.log(`Il peso minore è ${smallestWeight}Kg`);
+
 // Creare un array di oggetti di squadre di calcio.
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
